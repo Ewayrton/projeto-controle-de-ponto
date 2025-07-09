0ca1594e2635137fd802b4043508d3fae97a34cf
Ewayrton Brito de Oliveira <ewayrtonbrito@gmail.com>
2025-07-08 21:24:53 -0300

    criando projeto e navbar responsiva

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
+        <NavBar />{/*la ele */}
         {children}
       </body>
     </html>
